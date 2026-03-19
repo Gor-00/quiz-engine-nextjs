7746ca567a669096ae55f8e0705e893a4c933495
gor.mkrtchyan <gor.mkrtchyan@softconstruct.com>
2026-03-19 15:36:43 +0400

    build fix

--- lib/mongodbClient.ts
+++ lib/mongodbClient.ts
@@ -1,19 +1,19 @@
 import { MongoClient } from "mongodb";
 
-const uri = process.env.MONGODB_URI;
-
-if (!uri) {
-  throw new Error("MONGODB_URI is not set");
-}
-
 declare global {
   // eslint-disable-next-line no-var
   var mongoClientPromise: Promise<MongoClient> | undefined;
 }
 
-const mongoClient = new MongoClient(uri);
+function createMongoClientPromise() {
+  const uri = process.env.MONGODB_URI;
+  if (!uri) {
+    return Promise.reject(new Error("MONGODB_URI is not set"));
+  }
+  const mongoClient = new MongoClient(uri);
+  return mongoClient.connect();
+}
 
-const clientPromise =
-  global.mongoClientPromise ?? (global.mongoClientPromise = mongoClient.connect());
+const clientPromise = global.mongoClientPromise ?? (global.mongoClientPromise = createMongoClientPromise());
 
 export default clientPromise;
